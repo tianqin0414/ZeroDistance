773e3e139e606c5a1e399ba4fba6863ab7c7783c
ZeroIos <387956977@qq.com>
2018-08-29 15:15:55 +0800

    excessive

--- app/initialTQ/index.js
+++ app/initialTQ/index.js
@@ -1,17 +1,15 @@
 import React, { Component, } from 'react';
 import { StyleSheet, Text, View, } from 'react-native';
+import {createSwitchNavigator} from 'react-navigation';
 
 export default class Rule extends Component {
-	render () {
+	render() {
 		return (
-			<View>
+			<View >
 				<Text>
-				哈哈哈!
-				哈哈哈!
-				哈哈哈!
+				hahaah!
 				</Text>
 			</View>
-
 		);
 	}
 }
